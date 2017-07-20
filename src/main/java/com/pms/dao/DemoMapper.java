c479a84e1bcf36d2da543c1b7a034d4eabbb0bd9
Hanorz <[136111535@qq.com]>
2017-07-20 20:33:24 +0800

    创建了项目

---
+++ src/main/java/com/pms/dao/DemoMapper.java
@@ -0,0 +1,10 @@
+package com.pms.dao;
+
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by rhan on 2017/7/20.
+ */
+@Repository
+public interface DemoMapper {
+}
---
+++ src/main/test/BaseTest.java
@@ -0,0 +1,42 @@
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import com.pms.dao.DemoMapper;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.annotation.Resource;
+
+/**
+ * Created by rhan on 2017/7/20.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:springconfig.xml")
+public class BaseTest {
+    @Test
+    public void testRunWith()throws Exception{
+        System.out.println("hello spring Junit");
+    }
+
+    @Resource
+    ComboPooledDataSource comboPooledDataSource;
+    @Test
+    public void testDatapool()throws Exception{
+        comboPooledDataSource.getDescription();
+    }
+
+    @Resource
+    SqlSessionFactoryBean sqlSessionFactoryBean;
+    @Test
+    public void testSqlSessionFactory() throws Exception{
+        sqlSessionFactoryBean.isSingleton();
+    }
+
+    @Resource
+    DemoMapper demoMapper;
+    @Test
+    public void testMapperManager() throws Exception{
+
+    }
+}
