6dbea18be2c9ecb440162b9c7c5955b3097d2d86
Azaria_m <13882139946@163.com>
2017-10-10 22:01:40 +0800

    重新clone了一下

--- src/main/test/dlf/dnf/RoleControllerTest.java
+++ src/main/test/dlf/dnf/RoleControllerTest.java
@@ -1,16 +1,14 @@
 package dnf;
 
 import com.pms.model.user.User;
+import com.pms.service.user.UserService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import javax.annotation.Resource;
-
 import java.util.List;
-
-import static org.junit.Assert.*;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("classpath:springconfig.xml")
 public class RoleControllerTest {
